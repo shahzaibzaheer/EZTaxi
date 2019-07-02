c8cf208f0b586029c86c091e27d798ec10ea4cd7
Zaheer <zaheer.mughal1965@gmail.com>
2019-07-02 16:19:22 +0500

    rev slider added

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,56 @@
 $(document).ready(function () {
 
 
-
-
-
-
-
     // Nav open and clos
     const navItems = $('.main-nav .nav-items');
     $('#navOpenBtn').click(openNav);
     $('#navCloseBtn').click(closeNav);
 
 
-    function openNav(){
+    function openNav() {
         navItems.addClass('open');
         // console.log('open button clicked');
     }
-    function closeNav(){
+
+    function closeNav() {
         // console.log('close button clicked');
         navItems.removeClass('open');
     }
 
+
+
+    /* initialize the slider based on the Slider's ID attribute */
+    jQuery('#main_rev_slider').show().revolution({
+
+        /* options are 'auto', 'fullwidth' or 'fullscreen' */
+        sliderLayout: 'auto',
+        gridheight: 350,
+
+        responsiveLevels: [1240, 1024, 778, 480],
+        gridwidth:[1240, 1024, 778, 480],
+        gridheight:[450, 450, 450, 450],
+
+        /* basic navigation arrows and bullets */
+        navigation: {
+
+            arrows: {
+                enable: true,
+                style: 'hesperiden',
+                hide_onleave: true,
+            },
+
+            bullets: {
+                enable: true,
+                style: 'persephone',
+                hide_onleave: false,
+                h_align: 'center',
+                v_align: 'bottom',
+                h_offset: 0,
+                v_offset: 20,
+                space: 5
+            }
+        }
+    });
 
 
 });
